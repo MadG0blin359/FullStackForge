ad8181cca91eb3cfc426317b436d8cf625b4432c
MadG0blin359 <shawaizshahid312+github@gmail.com>
2025-10-09 21:19:13 +0500

    sect/ howitworks

--- Streamer - SaaS App/src/App.jsx
+++ Streamer - SaaS App/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   return (
     <main className="text-sm text-neutral-300 antialiased">
       <Navbar />
       <Hero />
+      <HowItWorks />
     </main>
   );
 };
