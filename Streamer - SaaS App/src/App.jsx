a2da03bd778341381df9efe220e98987b730b6c3
MadG0blin359 <shawaizshahid312+github@gmail.com>
2025-10-10 18:45:18 +0500

    sect/ key features

--- Streamer - SaaS App/src/App.jsx
+++ Streamer - SaaS App/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
+import KeyFeatures from "./components/KeyFeatures";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <HowItWorks />
+      <KeyFeatures />
     </main>
   );
 };
