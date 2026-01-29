fbbd7c704e8536c7ea5578f3fee53a9e5a70343c
MadG0blin359 <shawaizshahid312+github@gmail.com>
2026-01-29 16:10:47 +0500

    fix/ Remove Theme Toggle

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       {/* Background */}
       <StarBackground />
       <MeteorBackground />
-      <ThemeToggle />
+      {/* <ThemeToggle /> */}
       <Navbar />
 
       {/* Pages */}
